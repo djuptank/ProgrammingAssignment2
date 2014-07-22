5baae18e118749e263b6363bfd209c05cc0f4342
djuptank <janne.frank@me.com>
2014-07-22 22:18:53 +0200

    Added more comments in code.

--- cachematrix.R
+++ cachematrix.R
@@ -4,28 +4,37 @@
 ## Caching function
 
 makeCacheMatrix <- function(m = matrix()) {
+        # Start with empty matrix 
         m<-NULL 
+        
+        # Setting the matrix 
         set<-function(y){
-                x<<-y
+                x<<-y      # get from outside workspace
                 m<<-NULL
         }
+        # Getting the matrix
         get<-function() x
+        
+        # Solving function
         setMatrix<-function(solve) m<<- solve
         getMatrix<-function() m
+        
+        # Make list and output.
         list(set=set, get=get, setMatrix=setMatrix, getMatrix=getMatrix)
 }
 
 ## Solving
 
 cacheSolve <- function(x, ...) {
-        # ry 
+        # Get if exist and get
         m<-x$getMatrix()
         if(!is.null(m)){
                 message("getting cached data")
                 return(m)
         }
         matrix<-x$get()
-        m<-solve(matrix, ...)
+        m<-solve(matrix, ...) # Solve or pick up solution
         x$setMatrix(m)
+        # Return solution
         m
 }
